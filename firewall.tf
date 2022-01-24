90faac749203de58a03e8b2695aecdc0da1b8add
Praveen Muthu <praveen.mut@gmail.com>
2022-01-24 20:51:38 +1100

    Add in explicit firewall allow rules for bastion provisioning.

---
+++ firewall.tf
@@ -0,0 +1,18 @@
+## Allow access to Bastion host through SSH
+
+
+## East-west traffic between bastion to DB will not be necessary
+## as Google Cloud SQL Proxy will provide a secure tunnel to the
+## DB instance.
+resource "google_compute_firewall" "allow_ssh_to_bastion" {
+    name    = "allow-ssh-to-bastion"
+    network = "default"
+    direction = "INGRESS"
+
+    allow {
+        protocol = "tcp"
+        ports = ["22"]
+    }
+
+    source_tags = ["bastion"]
+} 
