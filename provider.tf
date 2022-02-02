5a5084efa88c9fbb384d4b9441d5037a215db9d3
Praveen Muthu <praveen.mut@gmail.com>
2022-02-03 02:09:12 +1100

    declare another aliased provider that essentially impersonates saBastion

--- provider.tf
+++ provider.tf
@@ -4,6 +4,17 @@ provider "google" {
   credentials = file(var.credentials)
 }
 
+provider "google" {
+  alias = "impersonator"
+  access_token = data.google_service_account_access_token.this.access_token
+  project = var.project
+  region = var.region
+  scopes = [
+    "https://www.googleapis.com/auth/cloud-platform",
+    "https://www.googleapis.com/auth/userinfo.email"
+  ]
+}
+
 provider "google-beta" {
   project     = var.project
   region      = var.region
