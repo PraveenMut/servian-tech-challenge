c89dae3592d8dc0ea9e4851a1fba6a1352563790
Praveen Muthu <praveen.mut@gmail.com>
2022-02-04 02:20:47 +1100

    Finialise the initialisation script

--- scripts/init.sh
+++ scripts/init.sh
@@ -11,10 +11,11 @@ cd ..
 mkdir -p .config
 
 # Create vars to specify relevant authN
-export PROJECT_NAME="servian-gtd"
-export TF_ADMIN="terraform-admin"
+export PROJECT_NAME="servian-gtd-application-tester"
+export TF_ADMIN="terraform-administrator"
 export TF_CREDS="${PWD}/.config/${TF_ADMIN}.json"
 export BILLING_ACCOUNT="$(gcloud beta billing accounts list | grep "True" | awk '{print $1}' | tr -d '\r\n')"
+export BUCKET_NAME="servian-terraform"
 
 # specify vars for terraform runs
 export GOOGLE_PROJECT="${PROJECT_NAME}"
@@ -37,26 +38,30 @@ echo "WARNING: THIS IS SENSITIVE DATA. DO NOT COMMIT THE PRIVATE KEY."
 gcloud iam service-accounts keys create ${TF_CREDS} \
     --iam-account ${TF_ADMIN}@${PROJECT_NAME}.iam.gserviceaccount.com
 
-echo "Add in IAM policies for the terraform account..."
-gcloud projects add-iam-policy-binding ${PROJECT_NAME} \
-    --member serviceAccount:${TF_ADMIN}@${PROJECT_NAME}.iam.gserviceaccount.com \
-    --role roles/editor
 
-gcloud projects add-iam-policy-binding ${PROJECT_NAME} \
-    --member serviceAccount:${TF_ADMIN}@${PROJECT_NAME}.iam.gserviceaccount.com \
-    --role roles/storage.admin
+# Wait for the service account gets propgated
+sleep 30
 
-gcloud projects add-iam-policy-binding ${PROJECT_NAME} \
-    --member serviceAccount:${TF_ADMIN}@${PROJECT_NAME}.iam.gserviceaccount.com \
-    --role roles/resourcemanager.projectIamAdmin
+echo "Adding IAM policies for the terraform account..."
 
-gcloud projects add-iam-policy-binding ${PROJECT_NAME} \
-    --member=serviceAccount:${TF_ADMIN}@${PROJECT_NAME}.iam.gserviceaccount.com \
-    --role=roles/servicenetworking.networksAdmin
+ROLES=(
+    "roles/editor" 
+    "roles/artifactregistry.admin" 
+    "roles/iam.serviceAccountAdmin" 
+    "roles/iam.serviceAccountTokenCreator" 
+    "roles/iam.workloadIdentityPoolAdmin" 
+    "roles/resourcemanager.projectIamAdmin" 
+    "roles/servicenetworking.networksAdmin" 
+    "roles/storage.admin"
+)
 
-gcloud projects add-iam-policy-binding ${PROJECT_NAME}  \
+for r in "${ROLES[@]}"
+do
+    echo "Adding IAM Policy Binding ${r}"
+    gcloud projects add-iam-policy-binding ${PROJECT_NAME} \
     --member serviceAccount:${TF_ADMIN}@${PROJECT_NAME}.iam.gserviceaccount.com \
-    --role roles/iam.workloadIdentityPoolAdmin
+    --role "${r}"
+done
 
 # Enable the serviceusage API to allow terraform to create IAM
 echo "Enable APIs to allow Terraform to interact with the underlying GCP REST APIs..."
@@ -72,8 +77,9 @@ SERVICES=(
     "artifactregistry.googleapis.com" 
     "sqladmin.googleapis.com" 
     "servicenetworking.googleapis.com" 
+    "iamcredentials.googleapis.com" 
+    "sts.googleapis.com"
     )
-
 for s in "${SERVICES[@]}"
 do
     echo "Enabling service ${s}"
@@ -82,10 +88,10 @@ done
 
 # Create bucket for remote state and set versioning
 echo "Create Bucket for remote TF state..."
-gsutil mb -p ${PROJECT_NAME} -l australia-southeast1 gs://servian-terraform 
-gsutil versioning set on gs://servian-terraform
+gsutil mb -p ${PROJECT_NAME} -l australia-southeast1 gs://${BUCKET_NAME}
+gsutil versioning set on gs://${BUCKET_NAME}
 
-# Init the backend
+#Init the backend
 echo "Initalising Terraform..."
 GOOGLE_APPLICATION_CREDENTIALS="${TF_CREDS}" terraform init
 
@@ -95,6 +101,11 @@ echo "|        Initialisation Complete.           |"
 echo "|-------------------------------------------|"
 echo ""
 echo "To provision underlying resources. Run terraform plan."
+echo "Terraform Plan can be run with exported environment variables."
+echo "Alternatively, you can use the provided convenience wrapper."
+echo "The convenience wrapper ensures that credentials aren't stored in the shell."
+echo "It also initalises the TF default credentials."
+echo "Usage ./terrawrapper.sh [plan|apply|output]"
 echo "If satisfactory, Run a terraform apply."
 
 echo "Afterwards, run the CI/CD pipeline."
