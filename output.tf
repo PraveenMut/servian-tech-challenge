1320ad81f7639c5e2efdf2171533f9d960657f85
Praveen Muthu <praveen.mut@gmail.com>
2022-02-07 02:56:20 +1100

    Allow the user to obtain the access details of the newly created bastion host.

--- output.tf
+++ output.tf
@@ -1,3 +1,20 @@
+## Obtain the workload identity provider ID
 output "workload_identity_pool_id" {
   value = module.gh_oidc.provider_name
+}
+
+## obtain the private key to ssh into the bastion instance
+output "private_key_pem" {
+  sensitive = true
+  value = tls_private_key.bastion
+}
+
+## obtain os login ssh user tied to the service account
+output "unique_id" {
+  value = "sa_${google_service_account.sa_bastion.unique_id}"
+}
+
+## obtain host IP
+output "host" {
+  value = google_compute_instance.bastion1.network_interface[0].access_config[0].nat_ip
 }
